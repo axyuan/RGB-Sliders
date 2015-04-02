0635c157b43f50a29ebb6bdc91965f7299b427d6
Ann Yuan <ayuan@thisisgrow.com>
2015-04-02 06:21:45 -0400

    unit 3

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -12,16 +12,49 @@ class ViewController: UIViewController {
     
     // first commit
 
+    @IBOutlet weak var swatch: UIView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        swatch.layer.borderColor = UIColor.blackColor().CGColor
+        swatch.layer.borderWidth = 1
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        redSlider.value = defaults.floatForKey("red")
+        greenSlider.value = defaults.floatForKey("green")
+        blueSlider.value = defaults.floatForKey("blue")
+        
+        updateBackgroundColor()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if(segue.identifier == "openColor") {
+            let newViewControler = segue.destinationViewController as UIViewController
+            newViewControler.view.backgroundColor = swatch.backgroundColor
+        }
+    }
+    
+    @IBAction func updateBackgroundColor() {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        swatch.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setFloat(redSlider.value, forKey: "red")
+        defaults.setFloat(greenSlider.value, forKey: "green")
+        defaults.setFloat(blueSlider.value, forKey: "blue")
+        defaults.synchronize()
+    }
 
 }
 
